b1946d32f953fc1fba0dfc9362ed738028d7a31f
Pankaj Gupta <pkg231098@gmail.com>
2023-06-15 18:05:08 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+       var url="https://api.chucknorris.io/jokes/random";
+       var request=HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+       var client= HttpClient.newBuilder().build();
+       var response=client.send(request, HttpResponse.BodyHandlers.ofString());
+       System.out.println(response.body());
+       System.out.println(response.statusCode());
+
+    }
+}
